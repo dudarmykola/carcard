84a5c658ed83ed11a380e45a56784cfa2ad3f7b6
dudarmykola <dudar.mykola.stepan@gmail.com>
2018-12-26 14:06:02 +0200

    Updates

---
+++ precache-manifest.8e1ee3192d62c55425f16e28e2191c9d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/carcard/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "5c9fc6afa08ff3500153",
+    "url": "/carcard/static/css/main.fb3d5e73.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/carcard/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "d555f74fe09afb518606",
+    "url": "/carcard/static/js/1.d555f74f.chunk.js"
+  },
+  {
+    "revision": "70418f095723343d3321",
+    "url": "/carcard/static/js/runtime~main.70418f09.js"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/carcard/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/carcard/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/carcard/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/carcard/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/carcard/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/carcard/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "5c9fc6afa08ff3500153",
+    "url": "/carcard/static/js/main.5c9fc6af.chunk.js"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/carcard/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/carcard/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/carcard/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/carcard/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/carcard/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/carcard/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/carcard/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/carcard/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "d555f74fe09afb518606",
+    "url": "/carcard/static/css/1.aea114eb.chunk.css"
+  },
+  {
+    "revision": "697a6d89aa2c99e90f1d4ac5cc8c121c",
+    "url": "/carcard/index.html"
+  }
+];
